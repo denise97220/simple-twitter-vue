81613c0e7226addc2d3c7131fbcd82e0b735230f
Dan <denise97220@gmail.com>
2021-07-04 22:14:34 +0800

    views setting

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+$mainColor: #FF6600;
 </style>
+
+<script>
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+export default {
+  
+}
+</script>
 
 
