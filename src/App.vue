1e7efda5904ecdb5cac2ab4d7c1b4ea5bfd7fe86
Dan <denise97220@gmail.com>
2021-07-04 22:42:58 +0800

    finish basic setting

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <style lang="scss">
-$mainColor: #FF6600;
+
 </style>
 
 <script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,76 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import UserLogin from "../views/UserLogin.vue";
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "root",
+    redirect: "/login",
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: "/login",
+    name: "user-login",
+    component: UserLogin,
+  },
+  {
+    path: "/regist",
+    name: "register",
+    component: () => import("../views/Register.vue"),
+  },
+  {
+    path: "/setting",
+    name: "setting",
+    component: () => import("../views/UserSetting.vue"),
+  },
+  {
+    path: "/main",
+    name: "user-main",
+    component: () => import("../views/UserSetting.vue"),
+  },
+  {
+    path: "/reply_list/:id",
+    name: "reply-list",
+    component: () => import("../views/ReplyList.vue"),
+  },
+  {
+    path: "/user/self",
+    name: "user-self",
+    component: () => import("../views/User.vue"),
+  },
+  {
+    path: "/user/self/follower",
+    name: "user-self-follower",
+    component: () => import("../views/UserSelfFollower.vue"),
+  },
+  {
+    path: "/user/self/following",
+    name: "user-self-following",
+    component: () => import("../views/UserSelfFollowing.vue"),
+  },
+  {
+    path: "/user/other/:id",
+    name: "user-other",
+    component: () => import("../views/OtherUser.vue"),
+  },
+  {
+    path: "/admin",
+    name: "admin-login",
+    component: () => import("../views/AdminLogin.vue"),
+  },
+  {
+    path: "/admin_main",
+    name: "admin-main",
+    component: () => import("../views/AdminMain.vue"),
+  },
+  {
+    path: "/admin_users",
+    name: "admin-users",
+    component: () => import("../views/AdminUsers.vue"),
+  },
+];
 
 const router = new VueRouter({
   routes
