3e79dcaf1c3cddad2caae8f40dcb84449114cb33
ashley huang <asashu81@hotmail.com>
2021-07-06 13:47:21 +0800

    Add UserProfile

---
+++ src/views/User.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <div class="user-navbar">
+      <Navbar />
+    </div>
+    <div class="user-container">
+      <div class="user-profile"><UserProfile /></div>
+      <div class="user-tweets"><UsersTweets /></div>
+    </div>
+
+    <div class="user-related"></div>
+  </div>
+</template>
+
+<script>
+import Navbar from "./../components/Navbar.vue";
+import UserProfile from "./../components/UserProfile.vue";
+import UsersTweets from "./../components/UserTweets.vue";
+
+export default {
+  name: "User",
+  components: {
+    Navbar,
+    UserProfile,
+    UsersTweets,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./src/assets/scss/main.scss";
+* {
+  font-style: normal;
+  font-weight: bold;
+}
+.container {
+  display: flex;
+  height: 100%;
+}
+.user-container {
+  width: 600px;
+
+  .user-profile {
+    height: 430px;
+  }
+  .user-tweets {
+    max-height: 720px;
+  }
+}
+</style>
